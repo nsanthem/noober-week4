4bec4df152a493a1bdf01fdc6ef4c04689220443
Tim Chung <tim.chung@kellogg.northwestern.edu>
2021-01-30 19:47:33 -0800

    added noober purple and noobler XL to work on html

--- noober.js
+++ noober.js
@@ -7,18 +7,21 @@ async function pageLoaded() {
   
   // 🔥 start here: write code to loop through the rides
   
+  console.log(json)
+
   let outputElement = document.querySelector('.rides')
   
   for (let i = 0; i < json.length; i++) { 
-    if(json[i].length > 1){
+    if(json[i].length > 1) {
       levelOfService = 'Noober Pool'
       outputElement.insertAdjacentHTML("beforeend",`
       <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
       <i class="fas fa-car-side"></i>
       <span>${levelOfService}</span>
+      </h1>
       `)
   
-  for (let x = 0; x < json[i].length; x++){
+  for (let x = 0; x < json[i].length; x++) {
     passengerName = `${json[i][x].passengerDetails.first} ${json[i][x].passengerDetails.last}`
     passengerPhone = `${json[i][x].passengerDetails.phoneNumber}`
     passengerNumber = `${json[i][x].numberOfPassengers} passengers`
@@ -27,34 +30,36 @@ async function pageLoaded() {
     dropoffAddress1 = `${json[i][x].dropoffLocation.address}`
     dropoffAddress2 = `${json[i][x].dropoffLocation.city}, ${json[i][x].dropoffLocation.state} ${json[i][x].dropoffLocation.zip}`
   
-    outputElement.insertAdjacentHTML("beforeend",`<div class="border-4 border-gray-900 p-4 my-4 text-left">
-      <div class="flex">
-        <div class="w-1/2">
-          <h2 class="text-2xl py-1">${passengerName}</h2>
-          <p class="font-bold text-gray-600">${passengerPhone}</p>
+    outputElement.insertAdjacentHTML("beforeend",`
+      <div class="border-4 border-gray-900 p-4 my-4 text-left">
+        <div class="flex">
+          <div class="w-1/2">
+            <h2 class="text-2xl py-1">${passengerName}</h2>
+            <p class="font-bold text-gray-600">${passengerPhone}</p>
+          </div>
+          <div class="w-1/2 text-right">
+            <span class="rounded-xl bg-gray-600 text-white p-2">
+              ${passengerNumber}
+            </span>
+          </div>
         </div>
-        <div class="w-1/2 text-right">
-          <span class="rounded-xl bg-gray-600 text-white p-2">
-            ${passengerNumber}
-          </span>
+        <div class="mt-4 flex">
+          <div class="w-1/2">
+            <div class="text-sm font-bold text-gray-600">PICKUP</div>
+            <p>${pickupAddress1}</p>
+            <p>${pickupAddress2}</p>
+          </div>
+          <div class="w-1/2">
+            <div class="text-sm font-bold text-gray-600">DROPOFF</div>
+            <p>${dropoffAddress1}</p>
+            <p>${dropoffAddress2}</p>
+          </div>
         </div>
       </div>
-      <div class="mt-4 flex">
-        <div class="w-1/2">
-          <div class="text-sm font-bold text-gray-600">PICKUP</div>
-          <p>${pickupAddress1}</p>
-          <p>${pickupAddress2}</p>
-        </div>
-        <div class="w-1/2">
-          <div class="text-sm font-bold text-gray-600">DROPOFF</div>
-          <p>${dropoffAddress1}</p>
-          <p>${dropoffAddress2}</p>
-        </div>
-      </div>
-      </div>`)
+      `)
       }
 
-    } else if (json[i].purpleRequested = 'true') {
+    } else if (json[i][0].purpleRequested) {
       levelOfService = 'Noober Purple'
       outputElement.insertAdjacentHTML("beforeend",`
       <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
@@ -63,7 +68,7 @@ async function pageLoaded() {
     </h1>
       `)
   
-  for (let x = 0; x < json[i].length; x++){
+  for (let x = 0; x < json[i].length; x++) {
     passengerName = `${json[i][x].passengerDetails.first} ${json[i][x].passengerDetails.last}`
     passengerPhone = `${json[i][x].passengerDetails.phoneNumber}`
     passengerNumber = `${json[i][x].numberOfPassengers} passengers`
@@ -101,7 +106,7 @@ async function pageLoaded() {
       `)
       }
 
-    } else if (json[i].numberOfPassengers > 3) {
+    } else if (json[i][0].numberOfPassengers > 3) {
       levelOfService = 'Noober XL'
       outputElement.insertAdjacentHTML("beforeend",`
       <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
@@ -110,7 +115,7 @@ async function pageLoaded() {
     </h1>
       `)
   
-  for (let x = 0; x < json[i].length; x++){
+  for (let x = 0; x < json[i].length; x++) {
     passengerName = `${json[i][x].passengerDetails.first} ${json[i][x].passengerDetails.last}`
     passengerPhone = `${json[i][x].passengerDetails.phoneNumber}`
     passengerNumber = `${json[i][x].numberOfPassengers} passengers`
@@ -148,7 +153,7 @@ async function pageLoaded() {
       `)
       }
 
-    } else levelOfService = 'Noober X'
+    } else {levelOfService = 'Noober X'
     outputElement.insertAdjacentHTML("beforeend",`
     <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
       <i class="fas fa-car-side"></i>
@@ -156,7 +161,7 @@ async function pageLoaded() {
     </h1>
     `)
   
-  for (let x = 0; x < json[i].length; x++){
+  for (let x = 0; x < json[i].length; x++) {
     passengerName = `${json[i][x].passengerDetails.first} ${json[i][x].passengerDetails.last}`
     passengerPhone = `${json[i][x].passengerDetails.phoneNumber}`
     passengerNumber = `${json[i][x].numberOfPassengers} passengers`
@@ -193,12 +198,9 @@ async function pageLoaded() {
       </div>
       `)
       }
-
+    }
 
   }  
-
-
-
 
   // end of code 
 
